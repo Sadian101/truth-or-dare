3c8df3db4efdef767872f670724df5bc25d15023
Sadian101 <112980503+Sadian101@users.noreply.github.com>
2022-09-13 12:18:38 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,52 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    random = randint(0, 1)
+    if (random == 0) {
+        basic.showString("Truth")
+    } else if (random == 1) {
+        basic.showString("Dare")
+    } else {
+    	
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    turn = randint(1, 4)
+    if (turn == 4) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (turn == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (turn == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (turn == 3) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else {
+    	
+    }
+})
+let turn = 0
+let random = 0
+basic.showString("Truth or Dare?")
